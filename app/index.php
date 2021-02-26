fe0026ec113f60fe889aa10f69028dc7a4674045
Simon Weber <simon.weber@sitewerk.ch>
2021-02-26 15:32:32 +0100

    bob is great

--- app/index.php
+++ app/index.php
@@ -53,7 +53,7 @@
     <div class="container">
 
       <div class="starter-template">
-        <h1>Hallo, das ist ja wunderbar <?php echo 'APPUiO'?></h1>
+        <h1>Bob is great <?php echo 'APPUiO'?></h1>
         <p class="lead">APPUiO Example Dockerfile PHP</p>
       </div>
 
